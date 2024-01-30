90cad563cc6e4f778c24472afa13dd9814e0d46b
Colleen Blake <colleenemilyblake@gmail.com>
2024-01-29 19:12:27 -0600

    creating table to connect to DB

--- MomAndPopShop/Data/ApplicationDbContext.cs
+++ MomAndPopShop/Data/ApplicationDbContext.cs
@@ -1,8 +1,10 @@
 ﻿using Duende.IdentityServer.EntityFramework.Options;
+using IdentityServer4.EntityFramework.Options;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using MomAndPopShop.Models;
+using OperationalStoreOptions = Duende.IdentityServer.EntityFramework.Options.OperationalStoreOptions;
 
 namespace MomAndPopShop.Data
 {
---
+++ MomAndPopShop/Models/CartDetail.cs
@@ -0,0 +1,6 @@
+﻿namespace MomAndPopShop.Models
+{
+    public class CartDetail
+    {
+    }
+}
