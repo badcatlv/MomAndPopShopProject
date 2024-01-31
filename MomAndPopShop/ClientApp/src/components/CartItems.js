34c737b691276556697906f5b122a4af1872c837
Colleen Blake <colleenemilyblake@gmail.com>
2024-01-31 06:57:42 -0600

    Connect database backend with front end successfully! Edit setuporxy.js, spproute.js and navmenu.js in react to add api connection as well as create component with connection to controller and model.

--- MomAndPopShop/ClientApp/src/components/CartItems.js
+++ MomAndPopShop/ClientApp/src/components/CartItems.js
@@ -19,7 +19,9 @@ const CartItems = () => {
     return (
         <main>
             {
-                (cartItems > 0) ? cartItems.map((cartItem) => <div>{cartItem.productName}</div>) : <div>Loading...</div>
+                (cartItems.length > 0) ? cartItems.map((cartItem) => <h3>{cartItem.popcornName}</h3>) : <div>Loading...</div>
+                                //cartItems.map((cartItem) => <h3>{cartItem.popcornName}</h3>)
+
             }
         </main>
     )
