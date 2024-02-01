afd0213ebdca3bfef3af0e946fd33b1459b16a2b
ChristinaMartin13 <christina13martin@gmail>
2024-01-31 20:51:21 -0600

    Made a new branch that includes ReactJS. Performed migrations and a database update. Registered as a user and can view the entire webpage. Also able to connect to the databse.

---
+++ MomAndPopShop/Migrations/20240201022403_InitialMigrationReact.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MomAndPopShop.Migrations
+{
+    public partial class InitialMigrationReact : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
