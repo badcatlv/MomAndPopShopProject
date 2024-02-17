e89d3c58e8db97f95801e6e009eb07642f3b3401
Colleen Blake <colleenemilyblake@gmail.com>
2024-02-17 14:29:00 -0600

    Stripe API and project are talking with eachother, still having issues opening up a checkout page, but the product list hosted by Stripe does appear, annonyingly on every p[age because its in the App.js file currently

--- MomAndPopShop/ClientApp/src/App.js
+++ MomAndPopShop/ClientApp/src/App.js
@@ -1,11 +1,74 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
 import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
 import { Layout } from './components/Layout';
 import './custom.css';
 
-export default class App extends Component {
+//import "./App.css";
+
+const ProductDisplay = () => (
+    <section>
+        <div className="product">
+            <img
+                src="https://i.imgur.com/EHyR2nP.png"
+                alt="The cover of Stubborn Attachments"
+            />
+            <div className="description">
+                <h3>Stubborn Attachments</h3>
+                <h5>$20.00</h5>
+            </div>
+        </div>
+        <form action="/create-checkout-session" method="POST">
+            <button type="submit">
+                Checkout
+            </button>
+        </form>
+    </section>
+);
+
+const Message = ({ message }) => (
+    <section>
+        <p>{message}</p>
+    </section>
+);
+
+export default function App() {
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        // Check to see if this is a redirect back from Checkout
+        const query = new URLSearchParams(window.location.search);
+
+        if (query.get("success")) {
+            setMessage("Order placed! You will receive an email confirmation.");
+        }
+
+        if (query.get("canceled")) {
+            setMessage(
+                "Order canceled -- continue to shop around and checkout when you're ready."
+            );
+        }
+    }, []);
+
+    return message ? (
+        <Message message={message} />
+    ) : (
+        <>
+            <Layout>
+                <Routes>
+                    {AppRoutes.map((route, index) => {
+                        const { element, requireAuth, ...rest } = route;
+                        return <Route key={index} {...rest} element={requireAuth ? <AuthorizeRoute {...rest} element={element} /> : element} />;
+                    })}
+                </Routes>
+            </Layout>
+            <ProductDisplay />
+        </>
+    );
+}
+
+/*export default class App extends Component {
   static displayName = App.name;
 
   render() {
@@ -21,3 +84,4 @@ export default class App extends Component {
     );
   }
 }
+*/
