fd03ab8ed97449de7588210a79bf15c358a9f219
Colleen Blake <colleenemilyblake@gmail.com>
2024-03-27 19:30:43 -0500

    Initial commit for project

--- MomAndPopShop/ClientApp/src/components/Home.js
+++ MomAndPopShop/ClientApp/src/components/Home.js
@@ -10,7 +10,7 @@ export class Home extends Component {
       <div>
                   <h1>Welcome to the Mom and Pop Shop!</h1> 
                   <br></br>
-                  <h3>We are a small business that sells a variety of products.</h3>
+                  <h3>We are a small business that sells a variety of popcorns! Seasoned to perfection!</h3>
                   <br></br>
                   <p>Please take a look around and let us know if you have any <Link to="./ContactForm/Create">questions</Link>.</p>
               </div>
