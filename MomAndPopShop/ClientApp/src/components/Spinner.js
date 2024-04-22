0d5c733f059e869de1a91426c5a6f17004687ce3
Colleen Blake <colleenemilyblake@gmail.com>
2024-04-22 17:15:37 -0500

    Working on a loading feature

---
+++ MomAndPopShop/ClientApp/src/components/Spinner.js
@@ -0,0 +1,16 @@
+﻿import { useState } from 'react';
+import DotLoader from "react-spinners/DotLoader";
+
+
+export const Spinner = () => {
+    const [loading, setLoading] = useState(true);
+    let [color, setColor] = useState("#000");
+
+    return (
+        <div className="sweet-loading" >
+            
+            <DotLoader color={color} loading={loading} size={150} aria-label="Loading Spinner" data-testid="loader" />
+        </div>
+    );
+};
+export default Spinner;
